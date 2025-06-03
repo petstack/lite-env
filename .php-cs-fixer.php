22e436b49ca20a7e800c9a54107b770dd61c3c20
Vasili Kamrakou <diolektor@gmail.com>
2025-06-04 01:44:23 +0400

    Add PHP-CS-Fixer configuration comments detailing usage and rules

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * PHP CS Fixer Configuration
+ *
+ * This file configures the PHP CS Fixer tool to enforce coding standards
+ * across the project. It sets up rules based on PSR-12 standards and
+ * specifies which directories and files should be checked.
+ *
+ * Usage:
+ * - Run `vendor/bin/php-cs-fixer fix` to automatically fix coding standards
+ * - Run with `--dry-run` flag to see what would be fixed without making changes
+ * - Run with `--diff` flag to see the changes that would be made
+ *
+ * @see https://github.com/FriendsOfPHP/PHP-CS-Fixer
+ */
 
 $finder = PhpCsFixer\Finder::create()
     ->in([
